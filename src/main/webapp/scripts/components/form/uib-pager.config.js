e04c53477f34fca5d24de54bde1a809adc542913
zhangsj <sjzhang@thoughtworks.com>
2015-12-14 12:47:05 +0800

    init

---
+++ src/main/webapp/scripts/components/form/uib-pager.config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('jhipster1App')
+    .config(function (uibPagerConfig) {
+        uibPagerConfig.itemsPerPage = 20;
+        uibPagerConfig.previousText = '«';
+        uibPagerConfig.nextText = '»';
+    });
